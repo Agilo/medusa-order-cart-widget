3684468cbd355cbc0f99cbd7fd458305ba7e8b2f
Ante Primorac <ante@agilo.com>
2026-05-04 11:28:07 +0200

    update baseUrl in Medusa SDK initialization to use root path

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -3,7 +3,7 @@ import Medusa from "@medusajs/js-sdk";
 export const sdk = new Medusa({
   baseUrl:
     (typeof __BACKEND_URL__ !== "undefined" ? __BACKEND_URL__ : undefined) ||
-    "http://localhost:9000",
+    "/",
   debug: import.meta.env.DEV,
   auth: {
     type: "session",
